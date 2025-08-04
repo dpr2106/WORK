082081f61fc1448a24222a2a791c17744e103d30
dpr2106 <prashanthdugyala60@gmail.com>
2025-08-04 21:57:08 +0530

    This is my first change

---
+++ sample.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+if a > b:
+    print(f"{a} is greater than {b}")           
+else:
+    print(f"{b} is greater than or equal to {a}")
