89c3d5aa2f07f72c210d67774e603c7fea873746
dpr2106 <prashanthdugyala60@gmail.com>
2025-08-08 19:21:10 +0530

    This is my first program in C !

---
+++ sample_again.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+// This program is my first program! :)
+/*Yeah I am a beginner type of programmer and very new to C Programming Languaage*/
+{ 
+printf("India is my country\n");
+printf("I am from Telanagana!\n");
+int age =18;
+int year=2025;
+printf("I am %d years old in the year %d\n", year);
+printf("My age is currently %d" , age);
+
+
+return 0;
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+// This program is my first program! :)
+/*Yeah I am a beginner type of programmer and very new to C Programming Languaage*/
+{ 
+printf("India is my country\n");
+printf("I am from Telanagan!\n");
+int age =18;
+printf("My age is currently %d" , age);
+
+
+return 0;
+}
