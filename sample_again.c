4e132f7ee263bfd2ca6885c5168a820ab2a2d91c
dpr2106 <prashanthdugyala60@gmail.com>
2025-08-14 23:14:32 +0530

    :)

--- sample_again.c
+++ sample_again.c
@@ -1,33 +1,24 @@
 #include <stdio.h>
 int main()
 // This program is my first program! :)
-/*Yeah I am a beginner type of programmer and very new to C Programming Languaage*/
+// Yeah I am a beginner type of programmer and very new to C Programming Languaage
 { 
+    int no_of_apples = 0;
+    float quantity = 0.0f;
+    char name[39] = "";
+    char quality = '\0';
 
+    printf("Enter the number of apples you bought: ");
+    scanf("%d", &no_of_apples);
+    printf("You bought %d apples.\n", no_of_apples);
 
-int no_of_apples = 0;
-float quantity = 0.0f;
-char name[39] = "";
-char quality = '\0';
-printf("India is my country and i love it the most !\n");
-printf("Enter the number of apples you bought: ");
-scanf("%d" , &no_of_apples);
+    printf("Enter the quantity of apples in kg: ");
+    scanf("%f", &quantity);
+    printf("Quantity of apples: %.2f kg\n", quantity);
 
+    printf("Enter the name of the Buyer: ");
+    scanf("%s", name);
+    printf("Name of the Buyer: %s\n", name);
 
-printf("%d\n" , no_of_apples);
-printf("%f\n" , quantity);
-printf("%c\n" , name);
-printf("%s\n" , quality);
-
-
-
-
-
-
-
-
-
-
-
-return 0;
+    return 0;
 }
