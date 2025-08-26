2ffd67601fac8c925e3b94e9928c5bca4137882e
dpr2106 <prashanthdugyala60@gmail.com>
2025-08-27 00:35:54 +0530

    ahahhahahahahahahahahahah

--- Shopping_cart.c
+++ Shopping_cart.c
@@ -8,10 +8,10 @@ int currency = 0;
 float total = 0.0f;
 
 
-printf("What item would you like to buy from our supermarket :      ");
+printf("What item would you like to buy from our supermarket : ");
 scanf("%s" , &item);
    
-printf("What is the price of one item ? (tell me real price hehe)     ");
+printf("What is the price of one item ? (tell me real price hehe)     $");
 scanf("%f" , &price);
  
 printf("How many items would you like to buy of that type ?");
@@ -19,7 +19,7 @@ scanf("%d" , &quantity);
 
 total = price * quantity;
 
-printf("The total cost of your ordered %d %s is %.2f" , quantity , item , total);
+printf("The total cost of your ordered %d %s is  $%.2f" , quantity , item , total);
 
 
 
