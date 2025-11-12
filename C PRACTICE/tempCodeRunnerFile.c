ecffb762168c2e738bff9bac1ac5b1bccf3efc86
Prashanth <87235698+dpr2106@users.noreply.github.com>
2025-11-13 00:53:25 +0530

    a

--- C PRACTICE/tempCodeRunnerFile.c
+++ C PRACTICE/tempCodeRunnerFile.c
@@ -1,1 +1,21 @@
-printf("                    1. Cash Withdrawal          \n");
+#include <stdio.h>
+void func(char []);//global declaration of a function 
+int main(){
+    char arr[]= "hello";
+    printf("Before Function call : %s\n" , arr);
+    func(arr);
+    printf("After function call : %s", arr);
+    return 0;
+}
+void func(char arr[])
+{
+    int i = 0;
+    while(arr[i] != '\0')
+    {
+    
+        arr[i] = arr[i] - 32;
+         i++;
+    }
+    
+
+}
