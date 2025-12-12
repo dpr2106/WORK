6d1161159dc27ba509690a804efbc391955aacea
Prashanth <87235698+dpr2106@users.noreply.github.com>
2025-12-12 20:11:26 +0530

    Club

---
+++ PYTHON/course.py
@@ -0,0 +1,139 @@
+from tkinter import*
+from PIL import Image,ImageTk
+from tkinter import ttk
+class CourseClass:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Student Result Management System")
+        self.root.geometry("1200x480+80+170")
+        self.root.config(bg="white")
+        self.root.focus_force()
+        title=Label(self.root,
+                    text="Manage Course Details",
+                    font=("goudy old style",30,"bold"),
+                    bg="light blue",
+                    fg="black",).place(x=10,y=15,width=1180,height=35)
+        
+        self.var_course=StringVar()
+        self.var_duration=StringVar()
+        self.var_charges=StringVar()
+
+        lbl_coursename=Label(self.root,
+                            text="Course Name:",
+                            font=("goudy old style",15,"bold"),
+                            bg="white").place(x=10,y=60)   
+        lbl_duration=Label(self.root,
+                            text="Duration:",
+                            font=("goudy old style",15,"bold"),
+                            bg="white").place(x=10,y=100)
+        lbl_charges=Label(self.root,
+                            text="Charges:",
+                            font=("goudy old style",15,"bold"),
+                            bg="white").place(x=10,y=140)
+        lbl_description=Label(self.root,
+                            text="Description:",    
+                            font=("goudy old style",15,"bold"),
+                            bg="white").place(x=10,y=180)
+
+        self.txt_coursename=Entry(self.root,
+                            textvariable=self.var_course,                             
+                            text="Course Name:",
+                            font=("goudy old style",15,"bold"),
+                            bg="lightyellow")
+        self.txt_coursename.place(x=150,y=60,width=200)   
+
+
+        txt_duration=Entry(self.root,
+                            textvariable=self.var_duration,
+                            font=("goudy old style",15,"bold"),
+                            bg="lightyellow").place(x=150,y=100,width=200)
+        txt_charges=Entry(self.root,
+                            textvariable=self.var_charges,
+                            font=("goudy old style",15,"bold"),
+                            bg="lightyellow").place(x=150,y=140)
+        
+
+        self.txt_description=Text(self.root,
+                            font=("goudy old style",15,"bold"),
+                            bg="lightyellow")
+        self.txt_description.place(x=150,y=180,width=500,height=140)
+
+        self.btn_add=Button(self.root,
+                            text='SAVE',
+                            font=("goudy old style",15,"bold"),
+                            bg="#2196f3",
+                            fg="white",
+                            cursor="hand2")
+        self.btn_add.place(x=150,
+                           y=400,
+                           width=110,
+                           height=40)
+        
+
+
+
+        self.btn_update=Button(self.root,
+                               text='UPDATE',
+                            font=("goudy old style",15,"bold"),
+                            bg="#4caf50",
+                            fg="white",
+                            cursor="hand2")
+        self.btn_update.place(x=270,
+                              y=400,
+                              width=110,
+                              height=40)
+        
+
+        self.btn_delete=Button(self.root,
+                               text='DELETE',
+                            font=("goudy old style",15,"bold"),
+                            bg="#f44336",       
+                            fg="white",
+                            cursor="hand2")
+        self.btn_delete.place(x=390,
+                              y=400,
+                              width=110,
+                              height=40)
+        
+
+
+        self.btn_clear=Button(self.root,
+                               text='CLEAR',        
+                            font=("goudy old style",15,"bold"),
+                            bg="#607d8b",
+                            fg="white",
+                            cursor="hand2")
+        self.btn_clear.place(x=510,
+                              y=400,
+                              width=110,
+                              height=40)
+        
+        self.var_search=StringVar()        
+        lbl_search_coursename=Label(self.root,
+                            text="Course Name:",
+                            font=("goudy old style",15,"bold"),
+                            bg="white").place(x=720,y=60)
+        txt_search_coursename=Entry(self.root,
+                                    textvariable=self.var_course,
+                            font=("goudy old style",15,"bold"),
+                            bg="lightyellow").place(x=870,y=60,width=180)
+        btn_search=Button(self.root,
+                            text='SEARCH',
+                            font=("goudy old style",15,"bold"),
+                            bg="#03a9f4",
+                            fg="white",
+                            cursor="hand2").place(x=1070,
+                                                  y=60,
+                                                  width=120,
+                                                  height=28)
+        self.C_Frame=Frame(self.root,bd=2,relief=RIDGE)
+        self.C_Frame.place(x=720,y=100,width=470,height=340)
+
+        self.CourseTable=TreeView
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=CourseClass(root)
+    root.mainloop()
+        
+        
--- PYTHON/dashboard.py
+++ PYTHON/dashboard.py
@@ -1,5 +1,6 @@
 from tkinter import*
 from PIL import Image,ImageTk
+from course import CourseClass
 class RMS:
     def __init__(self,root):
         self.root=root
@@ -17,7 +18,7 @@ class RMS:
                     image=self.logo_dash,
                     font=("goudy old style",30,"bold"),
                     bg="light blue",
-                    fg="black").place(x=0,y=0,relwidth=1,height=69)
+                    fg="black",).place(x=0,y=0,relwidth=1,height=69)
         M_Frame=LabelFrame(self.root,
                                        text="Menus",
                                        font=("arial black",15),
@@ -37,7 +38,8 @@ class RMS:
                           font=("Arial black",14,"bold"),
                           bg="#003049",
                           fg="white",
-                          cursor="hand2").place(x=20,y=2,width=150,height=30)
+                          cursor="hand2",
+                          command=self.add_course).place(x=20,y=2,width=150,height=30)
         btn_student=Button(M_Frame,
                           text="Students",
                           font=("Arial black",14,"bold"),
@@ -69,6 +71,41 @@ class RMS:
                             fg="white",
                             cursor="hand2").place(x=910,y=2,width=150,height=30)
         
+
+        self.bg_img=Image.open("images/c3.jpg")
+        self.bg_img=img=self.bg_img.resize((920,350),Image.Resampling.LANCZOS)
+        self.bg_img=ImageTk.PhotoImage(self.bg_img)
+        self.lbl_bg=Label(self.root,
+                          image=self.bg_img).place(x=400,y=180,width=920,height=350)
+        
+
+
+        self.lbl_course=Label(self.root,
+                              text="Total Courses\n[ 0 ]",
+                              font=("goudy old style",20),bd=10,relief=RIDGE,bg="#e43b06",fg="white")
+        self.lbl_course.place(x=400,y=530,width=300,height=100)
+
+        
+        self.lbl_student=Label(self.root,
+                              text="Total Students\n[ 0 ]",
+                              font=("goudy old style",20),bd=10,relief=RIDGE,bg="#0676ad",fg="white")
+        self.lbl_student.place(x=710,y=530,width=300,height=100)
+
+        
+        self.lbl_result=Label(self.root,
+                              text="Total Results\n[ 0 ]",
+                              font=("goudy old style",20),bd=10,relief=RIDGE,bg="#038074",fg="white")
+        self.lbl_result.place(x=1020,y=530,width=300,height=100)
+
+    def add_course(self):
+        self.new_win=Toplevel(self.root)
+        self.new_obj=CourseClass(self.new_win)
+
+        footer=Label(self.root,
+                    text="SRMS - Student Result Management System\n Contact us for any technical issue : +91987xxxxx01",
+                    font=("goudy old style",15),
+                    bg="black",
+                    fg="white").pack(side=BOTTOM,fill=X)
 if __name__ == "__main__":
     root=Tk()
     obj=RMS(root)
